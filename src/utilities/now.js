1b9301ecebe111c487234133289dc2c29dab0ca4
mosesmbuthia <mbuthiamoses435@gmail.com>
2025-03-22 21:16:13 +0300

    merged changes

---
+++ src/utilities/now.js
@@ -0,0 +1,9 @@
+const now =()=>{
+    const hour=new Date().getHours();
+    if(hour<12)return "Morning"
+    if(hour<18)return "Afternoon"
+    else return "Evening"
+    
+    
+    }
+    export default now;
